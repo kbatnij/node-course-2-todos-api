409b4b8bfc3d776ba76d8a5b0e76b86b9d53afbe
Khaldon Batnij <Batnij.k@gmail.com>
2018-04-13 20:20:44 -0700

    feat: added a delete script and refactored

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,33 +1,35 @@
-//const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
-    if (err) {
-        return console.log('Could not connect to MongoDB server');
-    }
-    console.log('Connected to MongoDB server');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false,
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert todo: ', err);
-        }
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-    db.collection('Users').insertOne({
-        name: 'Khaldon Batnij',
-        age: 27,
-        location: 'Seattle, WA'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Could not insert user.\nError: ', err);
-        }
-        console.log(result.ops[0]._id.getTimestamp());
-    });
-
-    db.close();
+  db.close();
 });
